3ff2ee89c62745614a44f1d59388080fd93f3970
NiveskZ <krsmjogos@outlook.com>
2025-12-31 23:42:26 -0300

    exercicio: endpoint/test ola mundo

--- fastapi_z/app.py
+++ fastapi_z/app.py
@@ -1,10 +1,26 @@
 from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fastapi_z.schemas import Message
+
 app = FastAPI()
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/ola_mundo', response_class=HTMLResponse)
+def ola_mundo():
+    return """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -23,3 +23,10 @@ def test_root_deve_retornar_ola_mundo():
     # assert
     assert response.json() == {'message': 'Olá mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_html_ola_mundo():
+    response = client.get('/ola_mundo')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
