2c41b8694b1f7f2385e418fe0a780846f56f541b
NiveskZ <krsmjogos@outlook.com>
2026-01-08 23:20:21 -0300

    feat: Adiciona primeira migração do Alembic. Cria tabela de usuários

--- tests/test_app.py
+++ tests/test_app.py
@@ -60,9 +60,7 @@ def test_update_user(client):
 
 
 def test_delete_user(client):
-    response = client.delete(
-        '/users/1'
-    )
+    response = client.delete('/users/1')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
         'username': 'bob',
