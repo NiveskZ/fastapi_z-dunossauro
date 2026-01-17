a5b64d6675ab2af8db200d24ca165fa69fe5a89e
NiveskZ <krsmjogos@outlook.com>
2026-01-16 22:14:58 -0300

    exercicio: testes erro get_current_user

---
+++ tests/conftest.py
@@ -0,0 +1,89 @@
+from contextlib import contextmanager
+from datetime import datetime
+
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
+
+from fastapi_z.app import app
+from fastapi_z.database import get_session
+from fastapi_z.models import User, table_registry
+from fastapi_z.security import get_password_hash
+
+
+@pytest.fixture
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture
+def session():
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2025, 5, 20)):
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+        # Exercicio aula 04
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
+
+
+@pytest.fixture
+def user(session):
+    password = 'testtest'
+    user = User(
+        username='Teste',
+        email='teste@teste.com',
+        password=get_password_hash('testtest'),
+    )
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    user.clean_password = password
+
+    return user
+
+
+@pytest.fixture
+def token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+
+    return response.json()['access_token']
